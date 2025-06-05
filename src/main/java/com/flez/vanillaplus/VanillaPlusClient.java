6fe7f86d9221f65497008ea27adcf06bc5e24c3a
zFLeze <Snakekillervn@gmail.com>
2025-06-05 16:33:44 +0700

    Initial commit

---
+++ src/main/java/com/flez/vanillaplus/VanillaPlusClient.java
@@ -0,0 +1,10 @@
+package com.flez.vanillaplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanillaPlusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/flez/vanillaplus/VanillaPlusDataGenerator.java
@@ -0,0 +1,11 @@
+package com.flez.vanillaplus;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VanillaPlusDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
