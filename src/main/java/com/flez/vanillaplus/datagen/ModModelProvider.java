26cb26cad29d9a589b18c62b6210959505062605
zFLeze <Snakekillervn@gmail.com>
2025-06-08 17:44:19 +0700

    Initialized

--- src/main/java/com/flez/vanillaplus/datagen/ModModelProvider.java
+++ src/main/java/com/flez/vanillaplus/datagen/ModModelProvider.java
@@ -4,80 +4,84 @@ import com.flez.vanillaplus.block.ModBlocks;
 import com.flez.vanillaplus.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.block.Block;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
 import net.minecraft.data.client.Models;
-import net.minecraft.block.Block;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
         super(output);
     }
 
-    @Override
-    public void generateBlockStateModels(BlockStateModelGenerator generator) {
-        generator.registerSimpleCubeAll(ModBlocks.FIREPROOF_OAK_PLANKS);
-        registerLogPair(generator,
-                ModBlocks.FIREPROOF_OAK_LOG,
-                ModBlocks.FIREPROOF_STRIPPED_OAK_LOG,
-                ModBlocks.FIREPROOF_OAK_WOOD,
-                ModBlocks.FIREPROOF_STRIPPED_OAK_WOOD);
-
-        generator.registerSimpleCubeAll(ModBlocks.FIREPROOF_SPRUCE_PLANKS);
-        registerLogPair(generator,
-                ModBlocks.FIREPROOF_SPRUCE_LOG,
-                ModBlocks.FIREPROOF_STRIPPED_SPRUCE_LOG,
-                ModBlocks.FIREPROOF_SPRUCE_WOOD,
-                ModBlocks.FIREPROOF_STRIPPED_SPRUCE_WOOD);
-
-        generator.registerSimpleCubeAll(ModBlocks.FIREPROOF_DARK_OAK_PLANKS);
-        registerLogPair(generator,
-                ModBlocks.FIREPROOF_DARK_OAK_LOG,
-                ModBlocks.FIREPROOF_STRIPPED_DARK_OAK_LOG,
-                ModBlocks.FIREPROOF_DARK_OAK_WOOD,
-                ModBlocks.FIREPROOF_STRIPPED_DARK_OAK_WOOD);
-
-        generator.registerSimpleCubeAll(ModBlocks.FIREPROOF_MANGROVE_PLANKS);
-        registerLogPair(generator,
-                ModBlocks.FIREPROOF_MANGROVE_LOG,
-                ModBlocks.FIREPROOF_STRIPPED_MANGROVE_LOG,
-                ModBlocks.FIREPROOF_MANGROVE_WOOD,
-                ModBlocks.FIREPROOF_STRIPPED_MANGROVE_WOOD);
-
-        generator.registerSimpleCubeAll(ModBlocks.FIREPROOF_CHERRY_PLANKS);
-        registerLogPair(generator,
-                ModBlocks.FIREPROOF_CHERRY_LOG,
-                ModBlocks.FIREPROOF_STRIPPED_CHERRY_LOG,
-                ModBlocks.FIREPROOF_CHERRY_WOOD,
-                ModBlocks.FIREPROOF_STRIPPED_CHERRY_WOOD);
-
-        generator.registerSimpleCubeAll(ModBlocks.FIREPROOF_ACACIA_PLANKS);
-        registerLogPair(generator,
-                ModBlocks.FIREPROOF_ACACIA_LOG,
-                ModBlocks.FIREPROOF_STRIPPED_ACACIA_LOG,
-                ModBlocks.FIREPROOF_ACACIA_WOOD,
-                ModBlocks.FIREPROOF_STRIPPED_ACACIA_WOOD);
-
-        generator.registerSimpleCubeAll(ModBlocks.FIREPROOF_BAMBOO_PLANKS);
-        registerLogPair(generator,
-                ModBlocks.FIREPROOF_BAMBOO_LOG,
-                ModBlocks.FIREPROOF_STRIPPED_BAMBOO_LOG,
-                ModBlocks.FIREPROOF_BAMBOO_WOOD,
-                ModBlocks.FIREPROOF_STRIPPED_BAMBOO_WOOD);
-
-        generator.registerSimpleCubeAll(ModBlocks.FIREPROOF_BAMBOO_MOSAIC);
+    private void registerLogPair(BlockStateModelGenerator generator,
+                                Block log, Block wood,
+                                Block strippedLog, Block strippedWood) {
+        generator.registerLog(log).log(log).wood(wood);
+        generator.registerLog(strippedLog).log(strippedLog).wood(strippedWood);
     }
 
-    private void registerLogPair(BlockStateModelGenerator generator,
-                                 Block log,
-                                 Block strippedLog,
-                                 Block wood,
-                                 Block strippedWood) {
-        generator.registerLog(log)
-                .log(log)
-                .log(strippedLog)
-                .wood(wood)
-                .wood(strippedWood);
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator generator) {
+        // PLANKS
+        generator.registerSimpleCubeAll(ModBlocks.FIREPROOF_OAK_PLANKS);
+        generator.registerSimpleCubeAll(ModBlocks.FIREPROOF_SPRUCE_PLANKS);
+        generator.registerSimpleCubeAll(ModBlocks.FIREPROOF_DARK_OAK_PLANKS);
+        generator.registerSimpleCubeAll(ModBlocks.FIREPROOF_JUNGLE_PLANKS);
+        generator.registerSimpleCubeAll(ModBlocks.FIREPROOF_ACACIA_PLANKS);
+        generator.registerSimpleCubeAll(ModBlocks.FIREPROOF_MANGROVE_PLANKS);
+        generator.registerSimpleCubeAll(ModBlocks.FIREPROOF_BAMBOO_PLANKS);
+        generator.registerSimpleCubeAll(ModBlocks.FIREPROOF_BAMBOO_MOSAIC);
+
+        // LOGS & WOODS
+        registerLogPair(generator,
+                ModBlocks.FIREPROOF_OAK_LOG,
+                ModBlocks.FIREPROOF_OAK_WOOD,
+                ModBlocks.FIREPROOF_STRIPPED_OAK_LOG,
+                ModBlocks.FIREPROOF_STRIPPED_OAK_WOOD);
+
+        registerLogPair(generator,
+                ModBlocks.FIREPROOF_SPRUCE_LOG,
+                ModBlocks.FIREPROOF_SPRUCE_WOOD,
+                ModBlocks.FIREPROOF_STRIPPED_SPRUCE_LOG,
+                ModBlocks.FIREPROOF_STRIPPED_SPRUCE_WOOD);
+
+        registerLogPair(generator,
+                ModBlocks.FIREPROOF_DARK_OAK_LOG,
+                ModBlocks.FIREPROOF_DARK_OAK_WOOD,
+                ModBlocks.FIREPROOF_STRIPPED_OAK_LOG,
+                ModBlocks.FIREPROOF_STRIPPED_OAK_WOOD);
+
+        registerLogPair(generator,
+                ModBlocks.FIREPROOF_JUNGLE_LOG,
+                ModBlocks.FIREPROOF_JUNGLE_WOOD,
+                ModBlocks.FIREPROOF_STRIPPED_JUNGLE_LOG,
+                ModBlocks.FIREPROOF_STRIPPED_JUNGLE_WOOD);
+
+        registerLogPair(generator,
+                ModBlocks.FIREPROOF_ACACIA_LOG,
+                ModBlocks.FIREPROOF_ACACIA_WOOD,
+                ModBlocks.FIREPROOF_STRIPPED_ACACIA_LOG,
+                ModBlocks.FIREPROOF_STRIPPED_ACACIA_WOOD);
+
+        registerLogPair(generator,
+                ModBlocks.FIREPROOF_MANGROVE_LOG,
+                ModBlocks.FIREPROOF_MANGROVE_WOOD,
+                ModBlocks.FIREPROOF_STRIPPED_MANGROVE_LOG,
+                ModBlocks.FIREPROOF_STRIPPED_MANGROVE_WOOD);
+
+        registerLogPair(generator,
+                ModBlocks.FIREPROOF_BAMBOO_LOG,
+                ModBlocks.FIREPROOF_BAMBOO_WOOD,
+                ModBlocks.FIREPROOF_STRIPPED_BAMBOO_LOG,
+                ModBlocks.FIREPROOF_STRIPPED_BAMBOO_WOOD);
+
+        registerLogPair(generator,
+                ModBlocks.FIREPROOF_BIRCH_LOG,
+                ModBlocks.FIREPROOF_BIRCH_WOOD,
+                ModBlocks.FIREPROOF_STRIPPED_BIRCH_LOG,
+                ModBlocks.FIREPROOF_STRIPPED_BIRCH_WOOD);
     }
 
     @Override
