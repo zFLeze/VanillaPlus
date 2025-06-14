5d90e298a54f498530c83d79a712a2e74a507641
zFLeze <Snakekillervn@gmail.com>
2025-06-14 21:40:49 +0700

    Initialized.

--- src/main/java/com/flez/vanillaplus/VanillaPlus.java
+++ src/main/java/com/flez/vanillaplus/VanillaPlus.java
@@ -5,6 +5,8 @@ import com.flez.vanillaplus.item.ModItemGroups;
 import com.flez.vanillaplus.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
+import net.minecraft.block.Blocks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +16,8 @@ public class VanillaPlus implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		FlammableBlockRegistry registry = FlammableBlockRegistry.getDefaultInstance();
+		registry.add(Blocks.HAY_BLOCK, 10, 30);
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 		ModItemGroups.registerItemGroup();
--- src/main/java/com/flez/vanillaplus/mixin/ModifyFunctionsMixin.java
+++ src/main/java/com/flez/vanillaplus/mixin/ModifyFunctionsMixin.java
@@ -1,10 +1,18 @@
 package com.flez.vanillaplus.mixin;
 
-import com.jcraft.jorbis.Block;
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Block.class)
-public abstract class ModifyFunctionsMixin {
+@Mixin(Item.class)
+public class ModifyFunctionsMixin {
+    @Inject(method = "maxCount", at = @At("HEAD"))
+    private void onCount(CallbackInfo info) {
 
+    }
 }
